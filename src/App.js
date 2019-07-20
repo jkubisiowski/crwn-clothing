0a191969459e9d85fc0a7ccee9db39371c2d3d16
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-20 09:33:51 +0200

    Add collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
 
 const HatPage = () => (
@@ -13,8 +14,8 @@ function App() {
   return (
     <>
       <Switch>
-        <Route component={HatPage} path="/hats" exact={true} />
-        <Route component={HomePage} path="/" exact={false} />
+        <Route component={HomePage} path="/" exact={true} />
+        <Route component={ShopPage} path="/shop" exact={false} />
       </Switch>
     </>
   );
