5f3614cd7dcaedb75001b7dd7e896dfbd6b4fb52
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-20 10:45:50 +0200

    Add custom button

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
 export class SignIn extends Component {
   state = {
@@ -40,7 +41,7 @@ export class SignIn extends Component {
             label="password"
             handleChange={event => this.handleChange(event)}
           />
-          <input type="submit" value="Submit form" />
+          <CustomButton type="submit">SIGN IN</CustomButton>
         </form>
       </div>
     );
