5a51c6a0c0b7ca8b87f6ba3ba1854d37d053cd8a
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-23 18:01:02 +0200

    Add user action types

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
@@ -42,18 +42,26 @@ class App extends React.Component {
         <Switch>
           <Route component={HomePage} path="/" exact={true} />
           <Route component={ShopPage} path="/shop" exact={false} />
-          <Route component={SignInAndSignUpPage} path="/signin" exact={false} />
+          <Route
+            exact
+            path="/signin"
+            render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
+          />
         </Switch>
       </>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDipatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDipatchToProps
 )(App);
