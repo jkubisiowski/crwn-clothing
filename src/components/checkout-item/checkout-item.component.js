39d6a61b642752760f4f1d2c73c7ca6fe117d523
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-29 10:05:47 +0200

    Add possibility to remove items from cart

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './checkout-item.styles.scss';
+import { connect } from 'react-redux';
+import { removeItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ cartItem, cartItem: { name, imageUrl, price, quantity }, removeItem }) => {
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -10,9 +12,18 @@ const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">${price}</span>
-      <div className="remove-button">&#10006;</div>
+      <div className="remove-button" onClick={() => removeItem(cartItem)}>
+        &#10006;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -8,3 +8,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+};
