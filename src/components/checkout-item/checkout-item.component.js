10a7adf26dfc6e1d8d0b95beaf455f839b703f94
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2019-07-29 10:36:16 +0200

    Add possibility to increase and decrease cart items

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './checkout-item.styles.scss';
 import { connect } from 'react-redux';
-import { removeItem } from '../../redux/cart/cart.actions';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem, cartItem: { name, imageUrl, price, quantity }, removeItem }) => {
+const CheckoutItem = ({
+  cartItem,
+  cartItem: { name, imageUrl, price, quantity },
+  clearItemFromCart,
+  addItem,
+  removeItem
+}) => {
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">${price}</span>
-      <div className="remove-button" onClick={() => removeItem(cartItem)}>
+      <div className="remove-button" onClick={() => clearItemFromCart(cartItem)}>
         &#10006;
       </div>
     </div>
@@ -20,6 +34,8 @@ const CheckoutItem = ({ cartItem, cartItem: { name, imageUrl, price, quantity },
 };
 
 const mapDispatchToProps = dispatch => ({
+  clearItemFromCart: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
   removeItem: item => dispatch(removeItem(item))
 });
 
